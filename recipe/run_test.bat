391c080f1b762cd97e3aa53beba495e20410decf
Filipe Fernandes <ocefpaf@gmail.com>
2017-12-15 20:32:48 -0200

    ncap2 test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,5 +2,7 @@ cd data
 
 ncgen -o in.nc in.cdl
 
+ncap2 -O -v -s 'erf_one=float(gsl_sf_erf(1.0f));print(erf_one,"%g")' in.nc foo.nc
+
 ncks -H --trd -v one in.nc
 if errorlevel 1 exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,6 +3,8 @@
 ncgen -o data/in.nc data/in.cdl
 fin=data/in.nc
 
+ncap2 -O -v -s 'erf_one=float(gsl_sf_erf(1.0f));print(erf_one,"%g")' $fin fooo.nc
+
 ncks -H --trd -v one $fin
 
 ncks -O --rgr skl=skl_t42.nc \
