81c6ddc277388b5f8aa0cd85dc372080e2c56b96
Filipe Fernandes <ocefpaf@gmail.com>
2017-12-16 18:02:36 -0200

    remove patches and add exit status

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,8 +1,8 @@
 cd data
 
-ncgen -o in.nc in.cdl
+ncgen -o in.nc in.cdl || exit 1
 
-ncap2 -O -v -s 'erf_one=float(gsl_sf_erf(1.0f));print(erf_one,"%g")' in.nc foo.nc
+ncks -H --trd -v one in.nc || exit 1
 
-ncks -H --trd -v one in.nc
-if errorlevel 1 exit 1
+ncap2 -O -v -s 'erf_one=float(gsl_sf_erf(1.0f));print(erf_one,"%g")' in.nc foo.nc || exit 1
+
