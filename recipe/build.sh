1e2fc858eed3050b6b143b9edbe8c2ac3732e592
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 20:40:19 -0300

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 if [[ $(uname) == Darwin ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-  export LDFLAGS="$LDFLAGS -headerpad_max_install_names"
-  # OS X user won't enjoy OMP due to the bad decision in conda-forge to use clang.
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-  export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
   ARGS="--disable-openmp --disable-regex --disable-shared --disable-doc"
 elif [[ $(uname) == Linux ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
   ARGS="--enable-openmp --disable-dependency-tracking"
 fi
 
@@ -21,8 +12,8 @@ export NETCDF_ROOT=$PREFIX
 
 ./configure --prefix=$PREFIX $ARGS
 
-make
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+make -j$CPU_COUNT
+make check
 make install
 
 # We can remove this when we start using the new conda-build.
