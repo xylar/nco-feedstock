81bb60683f90a8f8c9cf3889451bff577a6bdfae
Charlie Zender <zender@uci.edu>
2021-05-09 17:30:30 -0700

    Try single quotes on new UDUnits test command for Windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,4 +8,4 @@ ncap2 -O -v -s "erf_one=float(gsl_sf_erf(1.0f));" in.nc foo.nc || exit 1
 
 ncks -v erf_one foo.nc || exit 1
 
-ncks -C -H --trd -s '%6.0f' -d time_udunits,'1999-12-08 18:00:0.0','1999-12-09 12:00:0.0',2 -v time_udunits in.nc
+ncks -C -H --trd -s "%6.0f" -d time_udunits,"1999-12-08 18:00:0.0","1999-12-09 12:00:0.0",2 -v time_udunits in.nc
