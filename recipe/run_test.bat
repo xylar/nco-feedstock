8e8a44e21b33812f4698f2cdd089e40cc14efddf
Charlie Zender <zender@uci.edu>
2021-05-13 12:31:01 -0700

    nco-feedstock add OpenMP diagnostic to run_test.[sh/bat]

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,6 +8,8 @@ ncap2 -O -v -s "erf_one=float(gsl_sf_erf(1.0f));" in.nc foo.nc || exit 1
 
 ncks -v erf_one foo.nc || exit 1
 
+ncks -O --thr_nbr=3 --rgr lat_nbr=2 --dbg=2 in.nc foo.nc || exit 1
+
 ncks --tst_udunits="5 meters",centimeters in.nc || exit 1
 
 ncks -r || exit 1
